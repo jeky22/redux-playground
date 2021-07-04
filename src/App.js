e06032dfdcc40d3976324337b3adb219a251ee79
jeky22 <qhzh33@gmail.com>
2021-07-04 17:36:29 +0900

    redux router 설정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import store, { history } from "./store";
+import MyRouter from "./routes/Router";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <MyRouter />
+      </ConnectedRouter>
+    </Provider>
   );
-}
+};
 
 export default App;
---
+++ src/routes/Router.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Header from "../components/Footer";
+import Footer from "../components/Header";
+
+const MyRouter = () => (
+  <>
+    <Header />
+    <h1>hiddd</h1>
+    <Footer />
+  </>
+);
+
+export default MyRouter;
