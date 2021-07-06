d886e46492ba9e424afc22e128a83b7bec2f7165
jeky22 <qhzh33@gmail.com>
2021-07-06 22:18:27 +0900

    init

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { ConnectedRouter } from "connected-react-router";
 import store, { history } from "./store";
 import MyRouter from "./routes/Router";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/custom.scss";
+
 const App = () => {
   return (
     <Provider store={store}>
